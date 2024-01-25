fc5921cd1b14be03c137f7963373ae754e7363f3
jook <joakimdanke@gmail.com>
2024-01-25 14:24:14 +0100

    dsds

--- adventcode2.py
+++ adventcode2.py
@@ -1,11 +1,14 @@
 with open("adventcode2.txt", "r") as file:
   all_lines =file.readlines()
+  counter = 0
   for rad in all_lines:
     rad = rad.split()
     rad.pop(0)
     game_number = rad[0].strip(":")
     print(game_number)
+
     rad.pop(0)
+    toggle = True
     for index, i in enumerate(rad):
       i.strip(";")
       i.strip(":")
@@ -14,5 +17,10 @@ with open("adventcode2.txt", "r") as file:
         antal = i
       else:
         farg = i
-      if int(antal) <13 and farg == "red":
-        print(antal + farg)
+      if int(antal) >12 and farg == "red":
+        toggle = False
+        print(rad)
+    if toggle == True:
+      counter += int(game_number)
+      
+print(counter)
