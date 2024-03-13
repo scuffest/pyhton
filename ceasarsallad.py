76cb1a2d4a0bba64dbd23e4acd3ced745fade8ed
mushybones <joakimdanke@gmail.com>
2024-03-13 10:17:20 +0100

    kod

--- ceasarsallad.py
+++ ceasarsallad.py
@@ -1,4 +1,15 @@
+meddelande = input("Vill du dekryptera eller inkryptera?")
 
-bokstaver = "abcdefghijklmnopqrstvwxyzåäö"
- 
- print()
+alfabetet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'å', 'ä', 'ö']
+print()
+
+def dekrypt():
+ dek = input("Text du vill dekryptera och key")
+ dek_split = dek.split()
+ print(dek_split)
+
+def inkrypt():
+ print("")
+
+if meddelande == "dekryptera":
+ dekrypt(meddelande)
