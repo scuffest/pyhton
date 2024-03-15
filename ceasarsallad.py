587e00335c2a27607bd030d6fa9d3fe87ef95e3f
mushybones <joakimdanke@gmail.com>
2024-03-15 14:25:19 +0100

    hyej

--- ceasarsallad.py
+++ ceasarsallad.py
@@ -1,37 +1,41 @@
 import random
 
-user_input = input("kryptera / dekryptera:")
-nyckel = input("Nyckel:")
-meddelande = input("Skriv ditt meddelande:")
+def filetmignon():
 
-if nyckel.isdigit():
-  nyckel = int(nyckel)
-  printed_nyckel = nyckel
+  user_input = input("kryptera / dekryptera:")
+  nyckel = input("Nyckel:")
+  meddelande = input("Skriv ditt meddelande:")
 
-else:
-  nyckel = random.randint(1,28)
-  printed_nyckel = nyckel
+  if nyckel.isdigit():
+    nyckel = int(nyckel)
+    printed_nyckel = nyckel
+
+  else:
+    nyckel = random.randint(1,28)
+    printed_nyckel = nyckel
 
 
-def kryptering(nyckel, meddelande):
-  alfabetet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "å", "ä", "ö"]
-  storaalfabetet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Å", "Ä", "Ö"]
-  hej = ""
+  def kryptering(nyckel, meddelande):
+    alfabetet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "å", "ä", "ö"]
+    storaalfabetet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Å", "Ä", "Ö"]
+    hej = ""
 
-  for character in meddelande:
-    if character.isupper():
-      bokstav =   (storaalfabetet.index(character)  +nyckel)   % 28
-      krypterad = storaalfabetet[bokstav]
-      hej += krypterad
-    elif character.islower():
-      bokstav = (alfabetet.index(character)  +nyckel ) % 28
-      krypterad = alfabetet[bokstav]
-      hej += krypterad
-    else:
-      hej += character
-  print(hej, printed_nyckel)
+    for character in meddelande:
+      if character.isupper():
+        bokstav = (storaalfabetet.index(character)+nyckel) % 28
+        krypterad = storaalfabetet[bokstav]
+        hej += krypterad
+      elif character.islower():
+        bokstav = (alfabetet.index(character)+nyckel) % 28
+        krypterad = alfabetet[bokstav]
+        hej += krypterad
+      else:
+        hej += character
+    print(hej, printed_nyckel)
 
-if user_input == "kryptera":
-  kryptering(nyckel, meddelande)
-elif user_input == "dekryptera":
-  kryptering(-nyckel, meddelande)
+  if user_input == "kryptera":
+    kryptering(nyckel, meddelande)
+  elif user_input == "dekryptera":
+    kryptering(-nyckel, meddelande)
+
+filetmignon()
